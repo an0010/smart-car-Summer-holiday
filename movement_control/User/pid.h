72cb7c9e2bc685590989d31757c7b375223ff29a
an0010 <@mails.com>
2024-07-11 14:28:25 +0800

    fix: pid.h 修复了一个bug

--- movement_control/User/pid.h
+++ movement_control/User/pid.h
@@ -9,7 +9,7 @@ void pid_closing_ball(int32_t E2, int32_t E3, int32_t ENC,
     int32_t* SUM_pid_speed_2,int32_t* SUM_pid_speed_3,
     int32_t* PWM2, int32_t *PWM3,
     int32_t last_ENC__1_2, int32_t last_ENC__1_3,
-    int32_t ball_x)；
+    int32_t ball_x);
 void queue_push(int32_t* queue, int32_t value, int32_t length);
 int queue_sum(int32_t* queue, int32_t length);
 #endif
